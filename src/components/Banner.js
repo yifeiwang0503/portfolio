db1dbbb86b377de30fa4aa4b63bf3392579cfeca
yifeiwang0503 <110696734+yifeiwang0503@users.noreply.github.com>
2024-11-07 19:20:02 -0500

    basic structure of the website

---
+++ src/components/Banner.js
@@ -0,0 +1,66 @@
+import {useState, useEffect} from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerImg from '../assets/img/header-img.svg'
+
+const Banner = () => {
+    // For typing animation:
+    // the word that is currently being displayed
+    const [loopNum, setLoopNum] = useState(0);
+    // true/false the word is being typed out / deleted
+    const [isDeleting, setIsDeleting] = useState(false);
+    // List of words that needs to display
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"]; 
+    // the component needs to know what text we are showing right now
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+        return () => {clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if(!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{'Hi I\'m Yifei! \n'}<br/><span className="wrap">{text}</span></h1>
+                        <p>The text here will be replaced</p>
+                        <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default Banner;
